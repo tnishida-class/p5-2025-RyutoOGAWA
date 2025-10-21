e78aab274116c24050bff9586d69dac18f28f2ff
RyutoOGAWA <xiaochuanliudou94@gmail.com>
2025-10-21 13:00:11 +0900

    Change section3-1

--- Q3-1/sketch.js
+++ Q3-1/sketch.js
@@ -1,6 +1,6 @@
 // 心臓の鼓動のようなアニメーション
 const cycle = 100; // 1周期のフレーム数
-let count; // 何フレーム目か
+let count = 0; // 何フレーム目か
 
 function setup(){
   createCanvas(200, 200);
@@ -8,13 +8,23 @@ function setup(){
 }
 
 function draw(){
-  background(160, 192, 255);
-  
-  let speed = 1; // アニメーションの速さ
-  // BLANK[2]
-  count = (count + speed) % cycle;
-
   let size = 50;
+  background(160, 192, 255);
+  count = (count + 1) % cycle;
+  if(keyIsPressed){
+    count = (count + 3) % cycle;
+  }
+  if(count < cycle / 2){
+    size = 50 + cycle - count; //150~100
+  }else{
+    size = 50 + count; //100~150
+  } 
+  
+  //let speed = 1; // アニメーションの速さ
+  // BLANK[2]
+  //count = (count + speed) % cycle;
+  //let size = 50;
   // BLANK[1] 1周期の前半は size が大きくなり、後半は小さくなる
+
   ellipse(width / 2, height / 2, size);
 }
--- Q3-2/sketch.js
+++ Q3-2/sketch.js
@@ -6,7 +6,7 @@ const g = 1;
 function setup(){
   createCanvas(windowWidth, windowHeight);
   x = width / 2;
-  y = height / 2;
+  y = height * 0.8 - height * 0.05;
   vx = 0;
   vy = 0;
 }
@@ -25,8 +25,17 @@ function draw(){
   rect(0, groundY, width, height - groundY);
 
   // BLANK[1] キャラクターの左右移動
+  if(keyIsDown(LEFT_ARROW)){x -= 5;}
+  if(keyIsDown(RIGHT_ARROW)){x += 5;}
+  if(keyIsDown("B".charCodeAt(0))){
+    if(keyIsDown(LEFT_ARROW)){vx -= 1;}
+    if(keyIsDown(RIGHT_ARROW)){vx += 1;}
+  }else{
+    vx = 0;
+  }
 
   // BLANK[2] 重力とジャンプ
+  //if(keyPressed(" ".charCodeAt(0))){vy = -10;}
 
   // 速くなりすぎないように制限
   vx = constrain(vx, -20, 20);
